401f4dbc49e4ee031e2f5efc36fd0232e7ccdfe6
Jephte Pierre <110998062+jpstayfocus@users.noreply.github.com>
2024-12-21 12:16:25 -0500

    Create Variables.py

---
+++ Basics/Variables.py
@@ -0,0 +1,14 @@
+#variable is a place holder where you can store values
+# Example of variables
+name = "Foo" # String
+age = 20 # number
+number = [1,2,4,5,6] # list
+pi =  3.14 # decimal
+
+# we can combine variables
+#name, age  =  "Foo", 20
+
+print (name)
+print (age)
+print(number)
+print (pi)
