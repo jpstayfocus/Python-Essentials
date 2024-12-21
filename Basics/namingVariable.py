602b75f3b39508a67f17b6ae6df9677265ed682f
Jephte Pierre <110998062+jpstayfocus@users.noreply.github.com>
2024-12-21 12:28:41 -0500

    Create namingVariable.py

---
+++ Basics/namingVariable.py
@@ -0,0 +1,32 @@
+# Naming Variables with python
+
+#A variable name must start with a letter or the underscore character
+#A variable name cannot start with a number
+#A variable name can only contain alpha-numeric characters and underscores (A-z, 0-9, and _ )
+#Variable names are case-sensitive (age, Age and AGE are three different variables)
+#A variable name cannot be any of the Python keywords.
+
+#legal variable name:
+name = "Foo"
+my_name = "Foo"
+_my_name = "Foo"
+myName = "Foo" #
+MYNAME = "Foo"
+myname2 = "Foo"
+
+# Illegal variable names:
+2myname = "Foo"
+my-name = "Foo"
+my name = "Foo"
+
+#Variable names with more than one word can be difficult to read.
+#There are several techniques you can use to make them more readable:
+#Camel Case
+
+There are several techniques you can use to make them more readable:
+
+MyFullName = "Foo"
+
+#Snake Case
+#Each word is separated by an underscore character:
+my_mull_name = "Foo"
